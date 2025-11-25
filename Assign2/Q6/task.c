2cb331ea5d320d05f2b484a3886558a1f92ef203
Malin Andréasson <malandre@kth.se>
2025-11-25 14:51:23 +0100

    q6 is done (but do makefile)

--- Assign2/Q6/task.c
+++ Assign2/Q6/task.c
@@ -2,45 +2,96 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 int num_threads = 0;
-void *thread_func(); /* the thread function */
+int strip_size;
+float *array; 
+
+void *thread_func(void *arg); /* the thread function */
 
 
 int main(int argc, char *argv[])
 {
 	num_threads = atoi(argv[1]);
 /* Initialize an array of random values */
+	int array_size = 1000000;
+    strip_size = array_size/num_threads;
+	array = (float *)malloc(sizeof(float) * array_size);
+	for (int i = 0; i < array_size; i++) {
+		array [i] = rand() % 2;
+	}
 /* Perform Serial Sum */
 	float sum_serial = 0.0;
 	double time_serial = 0.0;
+    
+    clock_t start, end;
+
+	int i;
+
+    start = clock(); /* Master thread adding the array*/
+	for (i = 0; i < array_size; i++)
+	{
+		sum_serial += array[i];
+	}
+	
+	    
 //Timer Begin
+    end = clock();
+    time_serial = ((double) (end - start)) / CLOCKS_PER_SEC;
 //Timer End
-	printf("Serial Sum = , time = %.3f \n", sum_serial, time_serial);
+	printf("Serial Sum = %.3f, time = %.3f \n", sum_serial, time_serial);
 	/* Create a pool of num_threads workers and keep them in workers */
 	pthread_t *workers;
+	workers = (pthread_t *)malloc(sizeof(pthread_t) * num_threads);
+    int thread_ids[num_threads];
 	double time_parallel = 0.0;
 	double sum_parallel = 0.0;
 	//Timer Begin
-	for (int i = 0; i < num_threads; i++) {
+    clock_t begin, stop;
+    
+    begin = clock();
+    
+	for (i = 0; i < num_threads; i++) {
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
-	pthread_create(&workers[i], &attr, thread_func, NULL);
+    thread_ids[i] = i;
+	pthread_create(&workers[i], &attr, thread_func, &thread_ids[i]);
 	}
-	for (int i = 0; i < num_threads; i++)
-	pthread_join(workers[i], NULL);
+
+	for (i = 0; i < num_threads; i++){
+		float *thread_result;
+		
+		pthread_join(workers[i], (void**)&thread_result);  // get return value
+		sum_parallel += *thread_result;
+	}
+	
 	//Timer End
-	printf("Parallel Sum = , time = %.3f \n", sum_parallel, time_parallel);
+    stop = clock();
+    time_parallel = ((double) (stop - begin)) / CLOCKS_PER_SEC;
+	printf("Parallel Sum = %.3f, time = %.3f \n", sum_parallel, time_parallel);
 	/*free up resources properly */
+    free(array);
 }
 
-void *thread_func() {
+void *thread_func(void *arg) {
 /* Assign each thread an id so that they are unique in range [0, num_thread -1
 ] */
-	int my_id;
+	int my_id = *((int *)arg);
+	printf("Hi im thread: %d \n", my_id);
+
+	int first, last;
+
+  	/* determine first and last rows of my strip */
+  	first = my_id*strip_size;
+  	last = first + strip_size - 1;
 /* Perform Partial Parallel Sum Here */
-	float my_sum = 0.0;
-	printf("Thread %d sum = %f\n", my_id, my_sum);
-	pthread_exit(0);
+	float *my_sum = malloc(sizeof(float));
+    *my_sum = 0.0;
+	for (int i = first; i <= last; i++) {
+		*my_sum += array[i];
+	}
+	printf("Thread %d sum = %f\n", my_id, *my_sum);
+    
+	pthread_exit(my_sum);
 }
--- Assign2/Q8/pthread_stack.c
+++ Assign2/Q8/pthread_stack.c
@@ -4,7 +4,9 @@
 #include <stdlib.h>
      
 int num_threads = 0;
-     
+int node_count = 0; //erik jobbade här
+pthread_mutex_t stack_mutex; //erik jobbade här
+
 typedef struct node { 
      int node_id;      //a unique ID assigned to each node
      struct node *next;
@@ -16,19 +18,40 @@ Node *top; // top of stack
 void push_mutex() { 
      Node *old_node;
      Node *new_node;
-     new_node = malloc(sizeof(Node)); 
-
+     new_node = malloc(sizeof(Node));
+     
      //update top of the stack below
+     pthread_mutex_lock(&stack_mutex); //erik jobbade här
+     old_node = top; //erik jobbade här
+     new_node->next = old_node; //erik jobbade här
      //assign a unique ID to the new node
+     new_node->node_id = node_count++; //erik jobbade här
+     top = new_node; //erik jobbade här
+     printf("Pushed node id: %d\n", new_node->node_id); //erik jobbade här
+     pthread_mutex_unlock(&stack_mutex); //erik jobbade här
 }
 
 int pop_mutex() { 
      Node *old_node;
      Node *new_node;
-
+     
      //update top of the stack below
+     pthread_mutex_lock(&stack_mutex); //erik jobbade här
 
-     return old_node->node_id;
+     if (top == NULL) { //erik jobbade här
+          pthread_mutex_unlock(&stack_mutex); //erik jobbade här
+          return -1; //erik jobbade här
+     }
+
+     old_node = top; //erik jobbade här
+     top = old_node->next; //erik jobbade här
+
+     pthread_mutex_unlock(&stack_mutex); //erik jobbade här
+
+     int old_node_id = old_node->node_id; //erik jobbade här
+     free(old_node); //erik jobbade här
+     printf("Pushed node id: %d\n", old_node_id); //erik jobbade här
+     return old_node_id; //erik jobbade här
 }
 
 /*Option 2: Compare-and-Swap (CAS)*/
@@ -68,16 +91,20 @@ void *thread_func(int opt) {
 int main(int argc, char *argv[])
 {
      num_threads = atoi(argv[1]);
+     top = NULL; //erik jobbade här
+     node_count = 0; //erik jobbade här
+     pthread_mutex_init(&stack_mutex, NULL); //erik jobbade här
+
 
      /* Option 1: Mutex */ 
      pthread_t *workers;
      for (int i = 0; i < num_threads; i++) { 
           pthread_attr_t attr;
           pthread_attr_init(&attr);
-          pthread_create(...); 
+          pthread_create(&workers[i], &attr, thread_func, 0); //erik jobbade här
      }
      for (int i = 0; i < num_threads; i++) 
-          pthread_join(...);
+          pthread_join(workers[i], NULL); //erik jobbade här
 
      //Print out all remaining nodes in Stack
      printf("Mutex: Remaining nodes \n");
