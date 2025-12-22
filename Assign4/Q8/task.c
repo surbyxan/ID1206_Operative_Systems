b344bd455d1ca2014dae53ea93529947bf68ef38
elosinHD <elosin@kth.se>
2025-12-22 21:51:19 +0100

    task d and e small change

--- Assign4/Q8/task.c
+++ Assign4/Q8/task.c
@@ -13,60 +13,34 @@
 
 int main() {
     int fd;
-    void *map_addr;
+    void *map_address;
     pid_t pid;
 
     fd = open(FILE_NAME, O_RDWR);
-
     pid = fork();
 
-    map_addr = mmap(NULL, FILE_SIZE,
+    map_address = mmap(NULL, FILE_SIZE,
                     PROT_READ | PROT_WRITE,
                     MAP_SHARED,
                     fd, 0);
 
     if (pid == 0) {
         // Child process
-
-        memcpy(map_addr, "01234", 5); //this can also simply be done directly from map_addr+4096 in a for loop but this is cleaner
-
-        //char *child_message = "01234";
-        // for (int i = 0; i < 5; i++) {
-        //     map_addr[i] = child_message[i];
-        // }
-
-        msync(map_addr, FILE_SIZE, MS_SYNC);
-
+        memcpy(map_address, "01234", 5);
+        msync(map_address, FILE_SIZE, MS_SYNC);
         char buffer[5];
-        memcpy(buffer, map_addr+4096, 5); //this can also simply be done directly from map_addr+4096 in a for loop but this is cleaner
-
-        // for (int i = 0; i < 5; i++) {
-        //     buffer[i] = map_addr[i+4096];
-        // }
-
+        memcpy(buffer, map_address+4096, 5);
         printf("Child Process ( PID: %d ); read from mmaped [4096]: %s\n",
-               getpid(), buffer);
+            getpid(), buffer);
     } else {
         // Parent process
-        memcpy(map_addr+4096,"56789", 5); //this can also simply be done directly from map_addr in a for loop but this is cleaner
-        msync(map_addr, FILE_SIZE, MS_SYNC);
-
-        //char *parent_message = "56789";
-        // for (int i = 0; i < 5; i++) {
-        //     map_addr[i+4096] = parent_message[i];
-        // }
-
+        memcpy(map_address+4096,"56789", 5);
+        msync(map_address, FILE_SIZE, MS_SYNC);
         wait(NULL);
-
         char buffer[5];
-        memcpy(buffer, map_addr, 5); //this can also simply be done directly from map_addr in a for loop but this is cleaner
-        
-        // for (int i = 0; i < 5; i++) {
-        //     buffer[i] = map_addr[i];
-        // }
-
+        memcpy(buffer, map_address, 5);
         printf("Parent Process ( PID: %d ); read from mmaped [0]: %s\n",
-               getpid(), buffer);
+            getpid(), buffer);
     }
 
     close(fd);
